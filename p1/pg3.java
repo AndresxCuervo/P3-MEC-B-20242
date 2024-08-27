47c888ac34541ace70a6a4ea8791ff39ec123d56
AndresxCuervo <andrescuervopalacios@gmail.com>
2024-08-27 15:00:37 -0500

    Add files via upload

---
+++ p1/pg3.java
@@ -0,0 +1,12 @@
+package com.mycompany.primerparcial;
+
+// con la misma clase crear un metodo puro que me permita sumar dos fracciones y que devuelva su resultado
+public class pg3 {
+    public static void main(String[] args) {
+      public void fraccion.suma (double x, double y){
+          double r=x+y;
+          
+          return r;
+      }
+    }
+}
