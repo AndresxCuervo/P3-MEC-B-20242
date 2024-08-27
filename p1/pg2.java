eff4e50a40bb02be652e6165b8c34792cc851fbe
AndresxCuervo <andrescuervopalacios@gmail.com>
2024-08-27 14:51:10 -0500

    Add files via upload

---
+++ p1/pg2.java
@@ -0,0 +1,10 @@
+package com.mycompany.primerparcial;
+
+import fraccion;
+// con la misma clase del punto anterior crear una sobrecarga para el constructor y crear un objeto para cada una de sus formas
+
+public class pg2 {
+    public static void main(String[] args) {
+      
+    }
+}
