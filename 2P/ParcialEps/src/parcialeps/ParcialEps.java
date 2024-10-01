45ab1feab5302a0ba61198fbe80ffe0de2a99f81
AndresxCuervo <andrescuervopalacios@gmail.com>
2024-10-01 14:27:03 -0500

    Add files via upload

--- 2P/ParcialEps/src/parcialeps/ParcialEps.java
+++ 2P/ParcialEps/src/parcialeps/ParcialEps.java
@@ -1,53 +1,36 @@
 package parcialeps;
+
+
 import javax.swing.*;
 import java.awt.*;
 
 public class ParcialEps {
+
     public static void main(String[] args) {
-        
-        SwingUtilities.invokeLater(() -> {
-            
-            JFrame frame = new JFrame("Eps");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(600, 400);
+        JFrame ventana = new JFrame("EPS");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(600, 400);
+        ventana.setLayout(new BorderLayout());
 
-            JPanel panel = new JPanel();
-            panel.setLayout(new GridBagLayout());
-            GridBagConstraints gbc = new GridBagConstraints();
-            
-            JPanel panelIzquierdo = new JPanel();
-            panelIzquierdo.setBackground(Color.LIGHT_GRAY);
-            gbc.fill = GridBagConstraints.BOTH;
-            gbc.weightx = 1.0;
-            gbc.weighty = 1.0;
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            panel.add(panelIzquierdo, gbc);
-            
-            JPanel panelDerecho = new JPanel();
-            panelDerecho.setLayout(new GridBagLayout());
-            gbc.weightx = 2.0;
-            gbc.gridx = 1;
-            panel.add(panelDerecho, gbc);
+        JPanel panelIzquierdo = new JPanel();
+        panelIzquierdo.setPreferredSize(new Dimension(200, 0));
 
-            JPanel panelSuperior = new JPanel();
-            panelSuperior.setBackground(Color.GRAY);
-            gbc.weighty = 1.0;
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.gridwidth = 1;
-            gbc.gridheight = 1;
-            panelDerecho.add(panelSuperior, gbc);
+        JPanel panelDerecho = new JPanel();
+        panelDerecho.setLayout(new BorderLayout());
 
-            JPanel panelInferior = new JPanel();
-            panelInferior.setBackground(Color.DARK_GRAY);
-            gbc.weighty = 2.0;
-            gbc.gridy = 1;
-            panelDerecho.add(panelInferior, gbc);
-            
-            frame.add(panel);
-            frame.setVisible(true);
-        });
+        JPanel panelSuperiorDerecho = new JPanel();
+        panelSuperiorDerecho.setPreferredSize(new Dimension(0, 133));
+
+        JPanel panelInferiorDerecho = new JPanel();
+
+        panelDerecho.add(panelSuperiorDerecho, BorderLayout.NORTH);
+        panelDerecho.add(panelInferiorDerecho, BorderLayout.CENTER);
+
+        ventana.add(panelIzquierdo, BorderLayout.WEST);
+        ventana.add(panelDerecho, BorderLayout.CENTER);
+
+        ventana.setVisible(true);
     }
 }
+
 
